c382dfbde9a409a6c9a0da4f75d7427f857253a5
Andres Vazquez <andres.vazquez.martin@gmail.com>
2022-04-23 10:30:31 +0100

    First commit

--- routes/meow.js
+++ routes/meow.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Publication = require('./../models/publication');
 const routeGuard = require('./../middleware/route-guard');
+const fileUpload = require('./../middleware/file-upload');
 
 const meowRouter = new express.Router();
 
@@ -10,13 +11,55 @@ meowRouter.get('/create', routeGuard, (req, res) => {
 });
 
 // POST - '/meow/create' - Handles new meow creation
-meowRouter.post('/create', routeGuard, (req, res, next) => {
-  const { message } = req.body;
-  // Call create method on Publication model
-  Publication.create({
-    message,
-    creator: req.user._id
-  })
+meowRouter.post(
+  '/create',
+  routeGuard,
+  fileUpload.single('picture'),
+  (req, res, next) => {
+    const { message } = req.body;
+    //IF there ius a picture,
+    // Store the url in the picture variable
+    let picture;
+    if (req.file) {
+      picture = req.file.path;
+    }
+
+    // Call create method on Publication model
+    Publication.create({
+      message,
+      picture,
+      creator: req.user._id
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
+
+// GET - '/meow/:id' - Loads meow from database, renders single meow page
+
+meowRouter.get('/meow/:id', (req, res, next) => {
+  const { id } = req.params;
+  Publication.findById(id)
+    .populate('creator')
+    .then((publication) => {
+      res.render('meow-single', { publication });
+    })
+
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// GET - '/meow/:id/edit' - Loads meow from database, renders meow edit page
+// POST - '/meow/:id/edit' - Handles edit form submission.
+// POST - '/meow/:id/delete' - Handles deletion.
+meowRouter.post('/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Publication.findByIdAndRemove(id)
     .then(() => {
       res.redirect('/');
     })
@@ -24,11 +67,5 @@ meowRouter.post('/create', routeGuard, (req, res, next) => {
       next(error);
     });
 });
-
-// GET - '/meow/:id' - Loads meow from database, renders single meow page
-
-// GET - '/meow/:id/edit' - Loads meow from database, renders meow edit page
-// POST - '/meow/:id/edit' - Handles edit form submission.
-// POST - '/meow/:id/delete' - Handles deletion.
 
 module.exports = meowRouter;
